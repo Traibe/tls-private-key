fc43dc0690f271d0c6b5e9f3956ac7ccc8369662
nickhamilton1980 <nicholasehamilton@gmail.com>
2019-12-27 05:56:48 +1100

    debug

--- main.tf
+++ main.tf
@@ -3,24 +3,24 @@ terraform {
 }
 
 resource "random_id" "name" {
-  count = "${var.create ? 1 : 0}"
+  count = var.create ? 1 : 0
 
   byte_length = 4
   prefix      = "${var.name}-"
 }
 
 resource "tls_private_key" "key" {
-  count = "${var.create ? 1 : 0}"
+  count = var.create ? 1 : 0
 
-  algorithm   = "${var.algorithm}"
-  rsa_bits    = "${var.rsa_bits}"
-  ecdsa_curve = "${var.ecdsa_curve}"
+  algorithm   = var.algorithm
+  rsa_bits    = var.rsa_bits
+  ecdsa_curve = var.ecdsa_curve
 }
 
 resource "null_resource" "download_private_key" {
-  count = "${var.create ? 1 : 0}"
+  count = var.create ? 1 : 0
 
   provisioner "local-exec" {
-    command = "echo '${tls_private_key.key.private_key_pem}' > ${format("%s.key.pem", random_id.name.hex)} && chmod ${var.permissions} ${format("%s.key.pem", random_id.name.hex)}"
+    command = "echo '${tls_private_key.key[count.index].private_key_pem}' > ${format("%s.key.pem", random_id.name[count.index].hex)} && chmod ${var.permissions} ${format("%s.key.pem", random_id.name[count.index].hex)}"
   }
 }
